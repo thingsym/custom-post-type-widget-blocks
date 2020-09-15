5ec06fe2426de2c5db626e2a66c54c7b901056ef
thingsym <thingsym@gmail.com>
2020-09-15 15:57:09 +0900

    fix: fix uri to https

--- custom-post-type-widget-blocks.php
+++ custom-post-type-widget-blocks.php
@@ -5,7 +5,7 @@
  * Description: Custom Post Type Widgets for the Block Editor (Gutenberg).
  * Version:     1.1.0
  * Author:      thingsym
- * Author URI:  http://www.thingslabo.com/
+ * Author URI:  https://www.thingslabo.com/
  * License:     GPL2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: custom-post-type-widget-blocks
