6985a5511d75fa0e771aaa8eade3dbaa3a14fd43
thingsym <thingsym@gmail.com>
2022-02-04 17:55:28 +0900

    refactor: improve code structure

--- src/blocks/latest-comments/class-custom-post-type-widget-blocks-latest-comments.php
+++ src/blocks/latest-comments/class-custom-post-type-widget-blocks-latest-comments.php
@@ -67,71 +67,81 @@ class Custom_Post_Type_Widget_Blocks_Latest_Comments {
 			)
 		);
 
-		$list_items_markup = '';
-		if ( ! empty( $comments ) ) {
-			// Prime the cache for associated posts. This is copied from \WP_Widget_Recent_Comments::widget().
-			$post_ids = array_unique( wp_list_pluck( $comments, 'comment_post_ID' ) );
-			_prime_post_caches( $post_ids, strpos( get_option( 'permalink_structure' ), '%category%' ), false );
+		$classnames[] = 'wp-block-latest-comments';
 
-			foreach ( $comments as $comment ) {
-				$list_items_markup .= '<li class="wp-block-custom-post-type-widget-blocks-latest-comments__comment wp-block-latest-comments__comment">';
-				if ( $attributes['displayAvatar'] ) {
-					$avatar = get_avatar(
-						$comment,
-						48,
-						'',
-						'',
-						[
-							'class' => 'wp-block-custom-post-type-widget-blocks-latest-comments__comment-avatar',
-						]
-					);
-					if ( $avatar ) {
-						$list_items_markup .= $avatar;
-					}
-				}
+		if ( empty( $comments ) ) {
+			$classnames[] = 'no-comments';
 
-				$list_items_markup .= '<article>';
-				$list_items_markup .= '<footer class="wp-block-custom-post-type-widget-blocks-latest-comments__comment-meta">';
-				$author_url         = get_comment_author_url( $comment );
-				if ( empty( $author_url ) && ! empty( $comment->user_id ) ) {
-					$author_url = get_author_posts_url( $comment->user_id );
-				}
+			$wrapper_attributes = get_block_wrapper_attributes( array( 'class' => implode( ' ', $classnames ) ) );
 
-				$author_markup = '';
-				if ( $author_url ) {
-					$author_markup .= '<a class="wp-block-custom-post-type-widget-blocks-latest-comments__comment-author" href="' . esc_url( $author_url ) . '">' . get_comment_author( $comment ) . '</a>';
-				} else {
-					$author_markup .= '<span class="wp-block-custom-post-type-widget-blocks-latest-comments__comment-author">' . get_comment_author( $comment ) . '</span>';
-				}
-
-				// `_draft_or_post_title` calls `esc_html()` so we don't need to wrap that call in
-				// `esc_html`.
-				$post_title = '<a class="wp-block-custom-post-type-widget-blocks-latest-comments__comment-link" href="' . esc_url( get_comment_link( $comment ) ) . '">' . $this->draft_or_post_title( $comment->comment_post_ID ) . '</a>';
-
-				$list_items_markup .= sprintf(
-					/* translators: 1: author name (inside <a> or <span> tag, based on if they have a URL), 2: post title related to this comment */
-					__( '%1$s on %2$s', 'custom-post-type-widget-blocks' ),
-					$author_markup,
-					$post_title
-				);
-
-				if ( $attributes['displayDate'] ) {
-					$list_items_markup .= sprintf(
-						'<time datetime="%1$s" class="wp-block-custom-post-type-widget-blocks-latest-comments__comment-date">%2$s</time>',
-						esc_attr( get_comment_date( 'c', $comment ) ),
-						/* @phpstan-ignore-next-line */
-						date_i18n( get_option( 'date_format' ), get_comment_date( 'U', $comment ) )
-					);
-				}
-				$list_items_markup .= '</footer>';
-				if ( $attributes['displayExcerpt'] ) {
-					$list_items_markup .= '<div class="wp-block-custom-post-type-widget-blocks-latest-comments__comment-excerpt">' . wpautop( get_comment_excerpt( $comment ) ) . '</div>';
-				}
-				$list_items_markup .= '</article></li>';
-			}
+			return sprintf(
+				'<div %1$s>%2$s</div>',
+				$wrapper_attributes,
+				__( 'No comments to show.', 'custom-post-type-widget-blocks' )
+			);
 		}
 
-		$classnames[] = 'wp-block-latest-comments';
+		$list_items_markup = '';
+		// Prime the cache for associated posts. This is copied from \WP_Widget_Recent_Comments::widget().
+		$post_ids = array_unique( wp_list_pluck( $comments, 'comment_post_ID' ) );
+		_prime_post_caches( $post_ids, strpos( get_option( 'permalink_structure' ), '%category%' ), false );
+
+		foreach ( $comments as $comment ) {
+			$list_items_markup .= '<li class="wp-block-custom-post-type-widget-blocks-latest-comments__comment wp-block-latest-comments__comment">';
+			if ( $attributes['displayAvatar'] ) {
+				$avatar = get_avatar(
+					$comment,
+					48,
+					'',
+					'',
+					[
+						'class' => 'wp-block-custom-post-type-widget-blocks-latest-comments__comment-avatar',
+					]
+				);
+				if ( $avatar ) {
+					$list_items_markup .= $avatar;
+				}
+			}
+
+			$list_items_markup .= '<article>';
+			$list_items_markup .= '<footer class="wp-block-custom-post-type-widget-blocks-latest-comments__comment-meta">';
+			$author_url         = get_comment_author_url( $comment );
+			if ( empty( $author_url ) && ! empty( $comment->user_id ) ) {
+				$author_url = get_author_posts_url( $comment->user_id );
+			}
+
+			$author_markup = '';
+			if ( $author_url ) {
+				$author_markup .= '<a class="wp-block-custom-post-type-widget-blocks-latest-comments__comment-author" href="' . esc_url( $author_url ) . '">' . get_comment_author( $comment ) . '</a>';
+			} else {
+				$author_markup .= '<span class="wp-block-custom-post-type-widget-blocks-latest-comments__comment-author">' . get_comment_author( $comment ) . '</span>';
+			}
+
+			// `_draft_or_post_title` calls `esc_html()` so we don't need to wrap that call in
+			// `esc_html`.
+			$post_title = '<a class="wp-block-custom-post-type-widget-blocks-latest-comments__comment-link" href="' . esc_url( get_comment_link( $comment ) ) . '">' . $this->draft_or_post_title( $comment->comment_post_ID ) . '</a>';
+
+			$list_items_markup .= sprintf(
+				/* translators: 1: author name (inside <a> or <span> tag, based on if they have a URL), 2: post title related to this comment */
+				__( '%1$s on %2$s', 'custom-post-type-widget-blocks' ),
+				$author_markup,
+				$post_title
+			);
+
+			if ( $attributes['displayDate'] ) {
+				$list_items_markup .= sprintf(
+					'<time datetime="%1$s" class="wp-block-custom-post-type-widget-blocks-latest-comments__comment-date">%2$s</time>',
+					esc_attr( get_comment_date( 'c', $comment ) ),
+					/* @phpstan-ignore-next-line */
+					date_i18n( get_option( 'date_format' ), get_comment_date( 'U', $comment ) )
+				);
+			}
+			$list_items_markup .= '</footer>';
+			if ( $attributes['displayExcerpt'] ) {
+				$list_items_markup .= '<div class="wp-block-custom-post-type-widget-blocks-latest-comments__comment-excerpt">' . wpautop( get_comment_excerpt( $comment ) ) . '</div>';
+			}
+			$list_items_markup .= '</article></li>';
+		}
 
 		if ( $attributes['displayAvatar'] ) {
 			$classnames[] = 'has-avatars';
@@ -142,21 +152,13 @@ class Custom_Post_Type_Widget_Blocks_Latest_Comments {
 		if ( $attributes['displayExcerpt'] ) {
 			$classnames[] = 'has-excerpts';
 		}
-		if ( empty( $comments ) ) {
-			$classnames[] = 'no-comments';
-		}
 
 		$wrapper_attributes = get_block_wrapper_attributes( array( 'class' => implode( ' ', $classnames ) ) );
 
-		return ! empty( $comments ) ? sprintf(
+		return sprintf(
 			'<ol %1$s>%2$s</ol>',
 			$wrapper_attributes,
 			$list_items_markup
-		) : sprintf(
-			'<div %1$s>%2$s</div>',
-			$wrapper_attributes,
-			__( 'No comments to show.', 'custom-post-type-widget-blocks' )
 		);
 	}
-
 }
