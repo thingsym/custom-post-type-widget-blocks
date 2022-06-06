b90ac35fcb822721aedf94c9365b57c8eac7e882
thingsym <thingsym@gmail.com>
2022-06-06 17:13:42 +0900

    perf: add get_year_link_custom_post_type and get_day_link_custom_post_type methods

--- src/blocks/archives/class-custom-post-type-widget-blocks-archives.php
+++ src/blocks/archives/class-custom-post-type-widget-blocks-archives.php
@@ -87,14 +87,18 @@ class Custom_Post_Type_Widget_Blocks_Archives {
 			$dropdown_args['echo'] = 0;
 
 			if ( ! $disable_get_links ) {
+				add_filter( 'year_link', [ $this, 'get_year_link_custom_post_type' ], 10, 2 );
 				add_filter( 'month_link', [ $this, 'get_month_link_custom_post_type' ], 10, 3 );
+				add_filter( 'day_link', [ $this, 'get_day_link_custom_post_type' ], 10, 4 );
 				add_filter( 'get_archives_link', [ $this, 'trim_post_type' ], 10, 1 );
 			}
 
 			$archives = wp_get_archives( $dropdown_args );
 
 			if ( ! $disable_get_links ) {
+				remove_filter( 'year_link', [ $this, 'get_year_link_custom_post_type' ] );
 				remove_filter( 'month_link', [ $this, 'get_month_link_custom_post_type' ] );
+				remove_filter( 'day_link', [ $this, 'get_day_link_custom_post_type' ] );
 				remove_filter( 'get_archives_link', [ $this, 'trim_post_type' ] );
 			}
 
@@ -157,14 +161,18 @@ class Custom_Post_Type_Widget_Blocks_Archives {
 		$archives_args['echo'] = 0;
 
 		if ( ! $disable_get_links ) {
+			add_filter( 'year_link', [ $this, 'get_year_link_custom_post_type' ], 10, 2 );
 			add_filter( 'month_link', [ $this, 'get_month_link_custom_post_type' ], 10, 3 );
+			add_filter( 'day_link', [ $this, 'get_day_link_custom_post_type' ], 10, 4 );
 			add_filter( 'get_archives_link', [ $this, 'trim_post_type' ], 10, 1 );
 		}
 
 		$archives = wp_get_archives( $archives_args );
 
 		if ( ! $disable_get_links ) {
+			remove_filter( 'year_link', [ $this, 'get_year_link_custom_post_type' ] );
 			remove_filter( 'month_link', [ $this, 'get_month_link_custom_post_type' ] );
+			remove_filter( 'day_link', [ $this, 'get_day_link_custom_post_type' ] );
 			remove_filter( 'get_archives_link', [ $this, 'trim_post_type' ] );
 		}
 
@@ -183,6 +191,170 @@ class Custom_Post_Type_Widget_Blocks_Archives {
 			$wrapper_attributes,
 			$archives
 		);
+	}
+
+	/**
+	 * Gets the year link for custom post type.
+	 *
+	 * Hooks to year_link
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @param string $old_yearlink
+	 * @param string $year
+	 *
+	 * @return string $new_yearlink
+	 */
+	public function get_year_link_custom_post_type( $old_yearlink, $year ) {
+		$posttype = $this->posttype;
+
+		if ( ! $year ) {
+			$year = current_time( 'Y' );
+		}
+
+		global $wp_rewrite;
+		$new_yearlink = $wp_rewrite->get_year_permastruct();
+
+		if ( ! empty( $new_yearlink ) ) {
+			$front = preg_replace( '/\/$/', '', $wp_rewrite->front );
+
+			$new_yearlink = str_replace( '%year%', $year, $new_yearlink );
+
+			if ( 'post' === $posttype ) {
+				$new_yearlink = home_url( user_trailingslashit( $new_yearlink, 'year' ) );
+			}
+			else {
+				$type_obj = get_post_type_object( $posttype );
+
+				# The priority of the rewrite rule: has_archive < rewrite
+				# See https://developer.wordpress.org/reference/functions/register_post_type/
+				$archive_name = $posttype;
+				if ( is_string( $type_obj->has_archive ) ) {
+					$archive_name = $type_obj->has_archive;
+				}
+				if ( is_bool( $type_obj->rewrite ) && $type_obj->rewrite === true ) {
+					$archive_name = $posttype;
+				}
+				else if ( is_array( $type_obj->rewrite ) ) {
+					if ( ! empty( $type_obj->rewrite['slug'] ) ) {
+						$archive_name = $type_obj->rewrite['slug'];
+					}
+				}
+
+				if ( $front ) {
+					$new_front = $type_obj->rewrite['with_front'] ? $front : '';
+					$new_yearlink = str_replace( $front, $new_front . '/' . $archive_name, $new_yearlink );
+					$new_yearlink = home_url( user_trailingslashit( $new_yearlink, 'month' ) );
+				}
+				else {
+					$new_yearlink = home_url( user_trailingslashit( $archive_name . $new_yearlink, 'year' ) );
+				}
+			}
+		}
+		else {
+			$new_yearlink = home_url( '?post_type=' . $posttype . '&m=' . $year );
+		}
+
+		/**
+		 * Filter a yearlink.
+		 *
+		 * @since 1.4.0
+		 *
+		 * @param string $new_yearlink
+		 * @param string $year
+		 * @param string $old_yearlink
+		 */
+		return apply_filters( 'custom_post_type_widget_blocks/archive/get_year_link_custom_post_type', $new_yearlink, $year, $old_yearlink );
+	}
+
+	/**
+	 * Gets the day link for custom post type.
+	 *
+	 * Hooks to day_link
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @param string $old_daylink
+	 * @param string $year
+	 * @param string $month
+	 * @param string $day
+	 *
+	 * @return string $new_daylink
+	 */
+	public function get_day_link_custom_post_type( $old_daylink, $year, $month, $day ) {
+		$posttype = $this->posttype;
+
+		if ( ! $year ) {
+			$year = current_time( 'Y' );
+		}
+		if ( ! $month ) {
+			$month = current_time( 'm' );
+		}
+		if ( ! $day ) {
+			$day = current_time( 'j' );
+		}
+
+		global $wp_rewrite;
+		$new_daylink = $wp_rewrite->get_day_permastruct();
+
+		if ( ! empty( $new_daylink ) ) {
+			$front = preg_replace( '/\/$/', '', $wp_rewrite->front );
+
+			$new_daylink = str_replace( '%year%', $year, $new_daylink );
+			$new_daylink = str_replace( '%monthnum%', zeroise( intval( $month ), 2 ), $new_daylink );
+			$new_daylink = str_replace( '%day%', zeroise( intval( $day ), 2 ), $new_daylink );
+
+			if ( 'post' === $posttype ) {
+				$new_daylink = home_url( user_trailingslashit( $new_daylink, 'day' ) );
+			}
+			else {
+				$type_obj = get_post_type_object( $posttype );
+
+				# The priority of the rewrite rule: has_archive < rewrite
+				# See https://developer.wordpress.org/reference/functions/register_post_type/
+				$archive_name = $posttype;
+				if ( is_string( $type_obj->has_archive ) ) {
+					$archive_name = $type_obj->has_archive;
+				}
+				if ( is_bool( $type_obj->rewrite ) && $type_obj->rewrite === true ) {
+					$archive_name = $posttype;
+				}
+				else if ( is_array( $type_obj->rewrite ) ) {
+					if ( ! empty( $type_obj->rewrite['slug'] ) ) {
+						$archive_name = $type_obj->rewrite['slug'];
+					}
+				}
+
+				if ( $front ) {
+					$new_front = $type_obj->rewrite['with_front'] ? $front : '';
+					$new_daylink   = str_replace( $front, $new_front . '/' . $archive_name, $new_daylink );
+					$new_daylink   = home_url( user_trailingslashit( $new_daylink, 'day' ) );
+				}
+				else {
+					$new_daylink = home_url( user_trailingslashit( $archive_name . $new_daylink, 'day' ) );
+				}
+			}
+		}
+		else {
+			$new_daylink = home_url( '?post_type=' . $posttype . '&m=' . $year . zeroise( $month, 2 ) . zeroise( $day, 2 ) );
+		}
+
+		/**
+		 * Filter a daylink.
+		 *
+		 * @since 1.4.0
+		 *
+		 * @param string $new_daylink
+		 * @param string $year
+		 * @param string $month
+		 * @param string $day
+		 * @param string $old_daylink
+		 */
+		return apply_filters( 'custom_post_type_widget_blocks/archive/get_day_link_custom_post_type', $new_daylink, $year, $month, $day, $old_daylink );
 	}
 
 	/**
